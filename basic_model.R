0edaec08c29513437b8f6bf9c06ba438e9c4d002
adamkucharski <adam.kucharski@lshtm.ac.uk>
2020-06-09 09:38:24 +0100

    Update scaling

--- basic_model.R
+++ basic_model.R
@@ -53,14 +53,22 @@ plotbypc <- function(R=1.2, k=0.3, q=1, d = 0.5, ps=seq(0.0,1,0.1), cs=seq(0.2,1
     cols2=terrain.colors(length(cs)+2)[1:length(cs)]
     cols3=terrain.colors(length(cs)+2)[1:length(cs)]
     
-    matplot(tertiary$forward_avert,x=ps,type="o",pch=19,lty=1,col=cols1,lwd=2,ylab="cases averted (forward tracing)",xlab="success rate of contact tracing",ylim=c(0,tertiary$base*1.02))
-    legend("topleft",legend=rev(paste("adherence = ",cs)),pch=19,col=rev(cols1),bty="n")
+    matplot(tertiary$forward_avert/tertiary$base,x=ps,type="o",pch=19,lty=1,col=cols1,lwd=2,ylab="proportion cases averted (forward tracing)",xlab="proportion of contacts traced",ylim=c(0,1.02))
+    legend("topleft",legend=rev(paste("reduction if traced = ",cs)),pch=19,col=rev(cols1),bty="n")
     abline(h=tertiary$base,lwd=2,lty=2)
-    matplot(tertiary$fwd_back_avert,x=ps,type="o",pch=19,lty=1,col=cols2,lwd=2,ylab="cases averted (forward + backward tracing)",xlab="success rate of contact tracing",ylim=c(0,tertiary$base*1.02))
-    legend("topleft",legend=rev(paste("adherence = ",cs)),pch=19,col=rev(cols1),bty="n")
+    matplot(tertiary$fwd_back_avert/tertiary$base,x=ps,type="o",pch=19,lty=1,col=cols2,lwd=2,ylab="proportion cases averted (forward + backward tracing)",xlab="proportion of contacts traced",ylim=c(0,1.02))
+    legend("topleft",legend=rev(paste("reduction if traced = ",cs)),pch=19,col=rev(cols1),bty="n")
     abline(h=tertiary$base,lwd=2,lty=2)
-    matplot(tertiary$backward_gain,x=ps,type="o",pch=19,lty=1,col=cols3,lwd=2,ylab="difference",xlab="success rate of contact tracing",ylim=c(0,tertiary$base*1.02))
-    legend("topleft",legend=rev(paste("adherence = ",cs)),pch=19,col=rev(cols1),bty="n")
+    matplot(tertiary$backward_gain/tertiary$base,x=ps,type="o",pch=19,lty=1,col=cols3,lwd=2,ylab="additional averted with backward tracing",xlab="proportion of contacts traced",ylim=c(0,1.02))
+    legend("topleft",legend=rev(paste("reduction if traced = ",cs)),pch=19,col=rev(cols1),bty="n")
     abline(h=tertiary$base,lwd=2,lty=2)
     return(tertiary)
 }
+
+par(mfrow=c(1,3),mar=c(3,3,1,1),mgp=c(2,0.6,0))
+plotbypc()
+
+dev.copy(png,paste0("plot1.png"),units="cm",width=20,height=12,res=150)
+#dev.copy(pdf,paste0(dir_pick,"Figure_1.pdf"),width=6,height=8)
+dev.off()
+
